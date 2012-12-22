52d658b7d5c365a9b6364005438c39365e5ee2e0
Kris Kowal <kris.kowal@cixar.com>
2012-12-21 19:09:32 -0500

    Simplify removeTree

--- fs-common.js
+++ fs-common.js
@@ -198,18 +198,14 @@ exports.update = function (exports, workingDirectory) {
             if (stat.isSymbolicLink()) {
                 return self.remove(path);
             } else if (stat.isDirectory()) {
-                var list = self.list(path);
-                return Q.when(list, function (list) {
+                return self.list(path)
+                .then(function (list) {
                     // asynchronously remove every subtree
-                    var done = list.reduce(function (prev, name) {
-                        var next = self.removeTree(self.join(path, name));
-                        // join next and prev
-                        return prev ? Q.when(prev, function () {
-                            return next;
-                        }) : next;
-                    }, false);
-                    return Q.when(done, function () {
-                        self.removeDirectory(path);
+                    return Q.all(list.map(function (name) {
+                        return self.removeTree(self.join(path, name));
+                    }))
+                    .then(function () {
+                        return self.removeDirectory(path);
                     });
                 });
             } else {
