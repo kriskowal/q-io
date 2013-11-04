b65cc0cd3c713a75c4a4a6f692d5df0fe577e792
François Frisch <francoisfrisch@gmail.com>
2013-11-04 14:17:09 -0800

    Use SymbolicLink rather than Symlink in API
    Rename followInsecureSymlinks to followInsecureSymbolicLinks. This
    now matches what is writhe in the readme and release notes.

--- http-apps/fs.js
+++ http-apps/fs.js
@@ -53,11 +53,6 @@ exports.FileTree = function (root, options) {
         return Q.when(root, function (root) {
             var path = fs.join(root, request.pathInfo.slice(1));
             return Q.when(fs.canonical(path), function (canonical) {
-                //TODO remove for 2.0.0
-                if (options.followInsecureSymlinks) {
-                    Deprecate.deprecationWarning("followInsecureSymlinks", "followInsecureSymbolicLinks");
-                    options.followInsecureSymbolicLinks = true;
-                }
                 if (!fs.contains(root, canonical) && !options.followInsecureSymbolicLinks)
                     return options.notFound(request, response);
                 if (path !== canonical && options.redirectSymbolicLinks)
